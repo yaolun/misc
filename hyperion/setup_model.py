ae7093cabb64b8665bb4c47b5b2d18ebc05c7226
Yao-Lun Yang <allenya@gmail.com>
2015-03-23 22:17:41 -0500

    .

--- hyperion/setup_model.py
+++ hyperion/setup_model.py
@@ -98,7 +98,10 @@ def setup_model(outdir,outdir_global,outname,params,dust_file,tsc=True,idl=False
     # Calculate the dust sublimation radius
     T_sub = 2000
     a     = 1   #in micron
-    d_sub = 2.9388e7*(a/0.1)**-0.2 * (4*np.pi*rstar**2*sigma*tstar**4/LS)**0.5 / T_sub**3 *AU
+    # realistic dust
+    # d_sub = 2.9388e7*(a/0.1)**-0.2 * (4*np.pi*rstar**2*sigma*tstar**4/LS)**0.5 / T_sub**3 *AU
+    # black body dust
+    d_sub = (LS/16./np.pi/sigma/AU**2*(4*np.pi*rstar**2*sigma*tstar**4/LS)/T_sub**4)**0.5
     # use the dust sublimation radius as the inner radius of disk and envelope
     R_disk_min = d_sub
     R_env_min  = d_sub
