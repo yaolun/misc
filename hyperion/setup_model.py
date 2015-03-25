c785971957d23abcf19989617bfb6844e2cafb4e
yaolun <allenya@gmail.com>
2015-03-24 21:42:06 -0500

    .

--- hyperion/setup_model.py
+++ hyperion/setup_model.py
@@ -50,12 +50,10 @@ def setup_model(outdir,outdir_global,outname,params,dust_file,tsc=True,idl=False
     dust_hy['g'] = dust_radmc['g'][ind]
     dust_hy['p_lin_max'] = 0*dust_radmc['wl'][ind]     # assume no polarization
 
+    d = HenyeyGreensteinDust(dust_hy['nu'], dust_hy['albedo'], dust_hy['chi'], dust_hy['g'], dust_hy['p_lin_max'])
     # dust sublimation option
     d.set_sublimation_temperature('slow', temperature=1600.0)
-
-    d = HenyeyGreensteinDust(dust_hy['nu'], dust_hy['albedo'], dust_hy['chi'], dust_hy['g'], dust_hy['p_lin_max'])
-    # dust sublimation does not occur
-    # d.set_sublimation_temperature(None)
+    #
     d.write(outdir+'oh5.hdf5')
     d.plot(outdir+'oh5.png')
     plt.clf()
