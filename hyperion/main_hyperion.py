5e89149ae94b78f0b551475aa4b28bee2a692099
Yao-Lun Yang <allenya@gmail.com>
2015-03-28 18:37:24 -0500

    .

--- hyperion/main_hyperion.py
+++ hyperion/main_hyperion.py
@@ -47,15 +47,18 @@ else:
     last_model_num = (last.split('M_env_dot')[0]).split('Model')[1].split()[0]
 
 model_num = str(int(last_model_num)+1)
-
-for i in range(0, len(params)):
-    params_dict = params[i]
+# 
+model_num = 47
+#
+# for i in range(0, len(params)):
+for i in range(0,9):
+    # params_dict = params[i]
     if not os.path.exists(outdir+'model'+str(int(model_num)+i)+'/'):
         os.makedirs(outdir+'model'+str(int(model_num)+i)+'/')
     outdir_dum = outdir+'model'+str(int(model_num)+i)+'/'
     # print out some information about the current calculating model
     print 'Model'+str(int(model_num)+i)
-    pprint(params_dict)
+    # pprint(params_dict)
     # calculate the initial dust profile
     wl_aper = [3.6, 4.5, 5.8, 8.0, 10, 16, 20, 24, 35, 70, 100, 160, 250, 350, 500, 850]
     # option to fix some parameter
