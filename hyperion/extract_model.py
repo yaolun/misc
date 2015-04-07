d768a76be960289797106ed2e99819c32c6021fe
Yao-Lun Yang <allenya@gmail.com>
2015-04-07 15:00:52 -0500

    .

--- hyperion/extract_model.py
+++ hyperion/extract_model.py
@@ -337,9 +337,9 @@ def extract_hyperion(filename,indir=None,outdir=None,dstar=178.0,wl_aper=None,sa
 	fig.savefig(outdir+print_name+'_cube_plot.png', format='png', dpi=300, bbox_inches='tight')
 	fig.clf()
 
-indir = '/Users/yaolun/bhr71/obs_for_radmc/'
-outdir = '/Users/yaolun/bhr71/hyperion/'
-wl_aper = [3.6, 4.5, 5.8, 8.0, 8.5, 9, 9.7, 10, 10.5, 11, 16, 20, 24, 35, 70, 100, 160, 250, 350, 500, 850]
-extract_hyperion('/Users/yaolun/test/model11.rtout',indir=indir,outdir='/Users/yaolun/test/',wl_aper=wl_aper)
+# indir = '/Users/yaolun/bhr71/obs_for_radmc/'
+# outdir = '/Users/yaolun/bhr71/hyperion/'
+# wl_aper = [3.6, 4.5, 5.8, 8.0, 8.5, 9, 9.7, 10, 10.5, 11, 16, 20, 24, 35, 70, 100, 160, 250, 350, 500, 850]
+# extract_hyperion('/Users/yaolun/test/model11.rtout',indir=indir,outdir='/Users/yaolun/test/',wl_aper=wl_aper)
 # extract_hyperion('/hyperion/best_model_bettyjo.rtout',indir=indir,outdir=outdir+'bettyjo/')
 # extract_hyperion('/hyperion/old_setup2.rtout',indir=indir,outdir=outdir)
