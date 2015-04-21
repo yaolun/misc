dc380261714b35f15062095d4d610a765333953c
Yao-Lun Yang <allenya@gmail.com>
2015-04-20 19:21:26 -0500

    .

--- hyperion/rho_cav.py
+++ hyperion/rho_cav.py
@@ -9,12 +9,14 @@ def rho_cav(t):
 	MS = const.M_sun.cgs.value
 	G  = const.G.cgs.value
 	AU = const.au.cgs.value
+	yr = 3600*24*365.
 
 	# variable setup
 	mstar = 0.05 * MS
 	rstar = 5 * RS
-	L_bol = 12 * LS
+	L_bol = 11.89 * LS
 	theta_cav = np.radians(30)
+	# theta_cav = np.radians(15)
 	v = 1e7
 	t = 3600.*24*365 * t
 	g2d = 100
@@ -25,6 +27,6 @@ def rho_cav(t):
 
 	V = (v*t)**3 * (4/3.*np.pi - 2*np.pi*np.cos(theta_cav) + 2*np.pi*np.cos(theta_cav)**2 - 2*np.pi/3.*np.cos(theta_cav)**3) + 6/7.*np.pi/c0**2*np.cos(theta_cav)**(7/3.)*(v*t)**(-2/3.)
 
-	return M_dot_o*t/V, v*t/AU
+	return M_dot_o*t/V, v*t/AU, M_dot_o/MS*yr, V
 
-print rho_cav(2.)
+print rho_cav(1.)
