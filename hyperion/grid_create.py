a5ea946f91f08b38949ae47f8741adda4c209b60
yaolun <allenya@gmail.com>
2015-09-29 19:13:31 -0500

    .

--- hyperion/grid_create.py
+++ hyperion/grid_create.py
@@ -58,8 +58,11 @@ home = os.path.expanduser('~')
 # 			   'age': [3e3]}
 
 # should run view angle & outflow cavity opening angle
-list_params = {'view_angle': [20,30,40,50,60],\
-			   'theta_cav': [10,15,20,25,30]}
+# list_params = {'view_angle': [20,30,40,50,60],\
+# 			   'theta_cav': [10,15,20,25,30]}
+list_params = {'view_angle': [25,35,45,55,65],\
+		   'theta_cav': [12.5,17.5,22.5,27.5,32.5]}
+		   
 
 # outdir = '/Users/yaolun/test/'
 outdir = home + '/programs/misc/hyperion/'
