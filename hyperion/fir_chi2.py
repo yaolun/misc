1d914f4702203f130510c5e8f31af4fc50827352
Yao-Lun Yang <allenya@gmail.com>
2016-02-17 00:53:09 -0600

    update test grid

--- hyperion/fir_chi2.py
+++ hyperion/fir_chi2.py
@@ -477,8 +477,8 @@ obs = '/Users/yaolun/bhr71/obs_for_radmc/'
 # For fitting the best age for p25 dust opactity
 array_list = [{'listpath': '/Users/yaolun/bhr71/hyperion/test/model_list.txt',
                'datapath': '/Users/yaolun/bhr71/hyperion/test/',
-               'model_num': np.arange(58,63)}]
+               'model_num': np.arange(63,73)}]
             #    'model_num':np.hstack((17,np.arange(19,34)))}]
-# keywords = {'col':['age'], 'label': [r'$\rm{t_{col}\,[10^{4}\,year]}$']}
-keywords = {'col':['tstar'], 'label': [r'$\rm{T_{\star}\,[K]}$']}
-fir_chi2_2d(array_list, keywords, obs, fixed=True, herschel_only=True, zoom_1d=[6000,6600])
+keywords = {'col':['age'], 'label': [r'$\rm{t_{col}\,[10^{4}\,year]}$']}
+# keywords = {'col':['tstar'], 'label': [r'$\rm{T_{\star}\,[K]}$']}
+fir_chi2_2d(array_list, keywords, obs, fixed=True, herschel_only=True, zoom_1d=[0,5])
