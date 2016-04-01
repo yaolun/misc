8f536634af02c5bc44e52c8d5adbc07a37bd7eef
Yao-Lun Yang <allenya@gmail.com>
2016-04-01 07:18:09 -0500

    update

--- hyperion/main_hyperion.py
+++ hyperion/main_hyperion.py
@@ -191,6 +191,6 @@ else:
                              outdir=outdir_dum,aperture=aperture,filter_func=True,obj=obj)
         else:
             for w in mono_wave:
-                hyperion_image(outdir_dum+'model'+str(i)+'.rtout', w, outdir_dum, 'model'+str(int(model_num)+i))
+                hyperion_image(outdir_dum+'model'+str(i)+'.rtout', w, outdir_dum, 'model'+str(i))
         if temp:
             temp_hyperion(outdir_dum+'model'+str(i)+'.rtout',outdir=outdir_dum)
