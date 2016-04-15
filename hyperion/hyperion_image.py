dbf1d21bb1dc0ed9d2054d3242c0afde885cf08b
Yao-Lun Yang <allenya@gmail.com>
2016-04-15 17:48:14 -0500

    update input table

--- hyperion/hyperion_image.py
+++ hyperion/hyperion_image.py
@@ -1,4 +1,5 @@
-def hyperion_image(rtout, wave, plotdir, printname, dstar=178., group=0, marker=0, size='full'):
+def hyperion_image(rtout, wave, plotdir, printname, dstar=178., group=0, marker=0,
+                    size='full', convolve=False, unit=None):
     # to avoid X server error
     import matplotlib as mpl
     mpl.use('Agg')
@@ -12,10 +13,13 @@ def hyperion_image(rtout, wave, plotdir, printname, dstar=178., group=0, marker=
 
     pc = const.pc.cgs.value
 
+    if unit == None:
+        unit = r'$\rm{log(I_{\nu})\,[erg\,s^{-1}\,cm^{-2}\,Hz^{-1}\,sr^{-1}]}$'
+
     m = ModelOutput(rtout)
 
     # Extract the image.
-    image = m.get_image(group=group, inclination=0, distance=dstar * pc, units='MJy/sr')
+    image = m.get_image(group=group, inclination=0, distance=dstar * pc, units='mJy')
     # Open figure and create axes
     fig = plt.figure(figsize=(8,8))
     ax = fig.add_subplot(111)
@@ -30,10 +34,17 @@ def hyperion_image(rtout, wave, plotdir, printname, dstar=178., group=0, marker=
 
     # Image in the unit of MJy/sr
     # Change it into erg/s/cm2/Hz/sr
-    factor = 1e-23*1e6
+    # factor = 1e-23*1e6
+    factor = 1
     # avoid zero in log
     # flip the image, because the setup of inclination is upside down
     val = image.val[::-1, :, iwav] * factor + 1e-30
+
+    if convolve:
+        from astropy.convolution import convolve, Gaussian2DKernel
+        img_res = 2*w/len(val[:,0])
+        kernel = Gaussian2DKernel(0.27/2.354/img_res)
+        val = convolve(val, kernel)
 
     if size != 'full':
         pix_e2c = (w-size/2.)/w * len(val[:,0])/2
@@ -44,8 +55,11 @@ def hyperion_image(rtout, wave, plotdir, printname, dstar=178., group=0, marker=
     # the min and max levels for the colorscale (remove for default values).
     # cmap = sns.cubehelix_palette(start=0.1, rot=-0.7, gamma=0.2, as_cmap=True)
     cmap = plt.cm.CMRmap
-    im = ax.imshow(np.log10(val), vmin= -20, vmax= -15,
+    # im = ax.imshow(np.log10(val), vmin= -20, vmax= -15,
+    #           cmap=cmap, origin='lower', extent=[-w, w, -w, w], aspect=1)
+    im = ax.imshow(val,
               cmap=cmap, origin='lower', extent=[-w, w, -w, w], aspect=1)
+    print val.max()
 
     # plot the marker for center position by default or user input offset
     ax.plot([0],[-marker], '+', color='ForestGreen', markersize=10, mew=2)
@@ -69,7 +83,7 @@ def hyperion_image(rtout, wave, plotdir, printname, dstar=178., group=0, marker=
     cb = fig.colorbar(im, cax=cax)
     cb.solids.set_edgecolor("face")
     cb.ax.minorticks_on()
-    cb.ax.set_ylabel(r'$\rm{log(I_{\nu})\,[erg\,s^{-1}\,cm^{-2}\,Hz^{-1}\,sr^{-1}]}$',fontsize=18)
+    cb.ax.set_ylabel(unit,fontsize=18)
     cb_obj = plt.getp(cb.ax.axes, 'yticklabels')
     plt.setp(cb_obj,fontsize=14)
     # fix the tick label font
@@ -86,7 +100,8 @@ def hyperion_image(rtout, wave, plotdir, printname, dstar=178., group=0, marker=
     fig.savefig(plotdir+printname+'_image_'+str(wave)+'.pdf', format='pdf', dpi=300, bbox_inches='tight')
     fig.clf()
 
-# rtout = '/Users/yaolun/bhr71/hyperion/model26.rtout'
-# wave = 24
-# plotdir = '/Users/yaolun/test/'
-# hyperion_image(rtout, wave, plotdir, 'BHR71', group=0, marker=0, size=100.)
+rtout = '/Users/yaolun/bhr71/hyperion/model40.rtout'
+wave = 870
+plotdir = '/Users/yaolun/test/'
+hyperion_image(rtout, wave, plotdir, 'BHR71', group=0, marker=0, size=5., convolve=True,
+        unit=r'$\rm{mJy/beam}$')
