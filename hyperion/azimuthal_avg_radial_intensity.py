0291014d26153023fc487fc249afdb49f3abf2a1
Yao-Lun Yang <allenya@gmail.com>
2016-05-09 17:24:54 -0500

    fix a bug

--- hyperion/azimuthal_avg_radial_intensity.py
+++ hyperion/azimuthal_avg_radial_intensity.py
@@ -95,7 +95,7 @@ def azimuthal_avg_radial_intensity(wave, imgpath, source_center, rtout, plotname
     # print some header info
     foo.write('# wavelength '+str(wave)+' um \n')
     foo.write('# image file '+os.path.basename(imgpath)+' \n')
-    foo.write('$ annulus width '+str(annulus_width)+' arcsec \n')
+    foo.write('# annulus width '+str(annulus_width)+' arcsec \n')
     # write profiles
     foo.write('r_in[arcsec] \t I \t I_err \t I_sim \t I_sim_err \n')
     for i in range(len(I)):
