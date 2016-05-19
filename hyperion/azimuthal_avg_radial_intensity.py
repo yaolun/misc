de6711ff9b42246778646c693dd226be802e6773
Yao-Lun Yang <allenya@gmail.com>
2016-05-19 17:20:42 -0500

    add flexibiliy on image unit

--- hyperion/azimuthal_avg_radial_intensity.py
+++ hyperion/azimuthal_avg_radial_intensity.py
@@ -48,8 +48,18 @@ def azimuthal_avg_radial_intensity(wave, rtout, plotname, dstar,
         coord = SkyCoord(source_center, unit=(u.hourangle, u.deg))
         pixcoord = w.wcs_world2pix(coord.ra.degree, coord.dec.degree, 1)
         pix2arcsec = abs(im_hdu[1].header['CDELT1'])*3600.
-        # convert intensity unit from MJy/sr to Jy/pixel
-        factor = 1e6/4.25e10*abs(im_hdu[1].header['CDELT1']*im_hdu[1].header['CDELT2'])*3600**2
+
+        # determine whether need to convert the unit
+        factor = 1
+        print 'Image unit is ', im_hdu[1].header['BUNIT']
+        if im_hdu[1].header['BUNIT'] != 'Jy/pixel':
+            print 'Image unit is ', im_hdu[1].header['BUNIT']
+
+            if im_hdu[1].header['BUNIT'] == 'MJy/sr':
+                # convert intensity unit from MJy/sr to Jy/pixel
+                factor = 1e6/4.25e10*abs(im_hdu[1].header['CDELT1']*im_hdu[1].header['CDELT2'])*3600**2
+            else:
+                factor = raw_input('What is the conversion factor to Jy/pixel?')
 
         I = np.empty_like(r[:-1])
         I_err = np.empty_like(r[:-1])
