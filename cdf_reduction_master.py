7b3735e8f95f046089cbd789492b33010cf10295
Yao-Lun Yang <allenya@gmail.com>
2017-05-01 17:40:17 -0500

    switch functions

--- cdf_reduction_master.py
+++ cdf_reduction_master.py
@@ -172,53 +172,53 @@ for element in header:
 #
 # ###################### STEP 3 ######################
 # line fitting on cube products
-idl('.r /home/bettyjo/yaolun/programs/line_fitting/extract_spire.pro')
-idl('.r /home/bettyjo/yaolun/programs/line_fitting/gauss.pro')
-idl('.r /home/bettyjo/yaolun/programs/line_fitting/plot_contour.pro')
+# idl('.r /home/bettyjo/yaolun/programs/line_fitting/extract_spire.pro')
+# idl('.r /home/bettyjo/yaolun/programs/line_fitting/gauss.pro')
+# idl('.r /home/bettyjo/yaolun/programs/line_fitting/plot_contour.pro')
 #
 # # for SPIRE
-foo = open(outdir+reduction_name+'_spire_lines_1d.txt', 'w')
-foo.write('{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s} \n'.format(*header))
-foo.close()
-#
+# foo = open(outdir+reduction_name+'_spire_lines_1d.txt', 'w')
+# foo.write('{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s} \n'.format(*header))
+# foo.close()
 # #
-# The option of wavelength shift for testing the false-positive rate
-if 'FPtest' in reduction_name:
-    wl_shift = 6.8
-else:
-    wl_shift = 0
-
-for o in obsid_spire:
-    # for running a certain source
-    if o != obsid_spire[obj_list_spire.index('HH100')]:
-        continue
-    obj = obj_list_spire[obsid_spire.index(o)]
-    print 'Step 3 - ', obj
-    # read in RA/Dec
-    radec_slw = ascii.read(outdir+obj+'/spire/data/cube/'+obj+'_radec_slw.txt')
-    radec_ssw = ascii.read(outdir+obj+'/spire/data/cube/'+obj+'_radec_ssw.txt')
-    # SLW
-    idl.pro('extract_spire', indir=outdir+obj+'/spire/data/cube/', outdir=outdir+obj+'/spire/advanced_products/cube/',
-            plotdir=outdir+obj+'/spire/advanced_products/cube/plots/', localbaseline=10, global_noise=20,
-            ra=radec_slw['RA(deg)'].data, dec=radec_slw['Dec(deg)'].data, coordpix=radec_slw['Pixel'].data,
-            slw=1, noiselevel=3, brightness=1, object=obj, flat=1, continuum=1, current_pix=1, double_gauss=1,
-            print_all=outdir+reduction_name+'_spire_lines', wl_shift=wl_shift)
-    # SSW
-    idl.pro('extract_spire', indir=outdir+obj+'/spire/data/cube/', outdir=outdir+obj+'/spire/advanced_products/cube/',
-            plotdir=outdir+obj+'/spire/advanced_products/cube/plots/', localbaseline=10, global_noise=20,
-            ra=radec_ssw['RA(deg)'].data, dec=radec_ssw['Dec(deg)'].data, coordpix=radec_ssw['Pixel'].data,
-            ssw=1, noiselevel=3, brightness=1, object=obj, flat=1, continuum=1, current_pix=1, double_gauss=1,
-            print_all=outdir+reduction_name+'_spire_lines', wl_shift=wl_shift)
-
-    # make contour plots
-    idl.pro('plot_contour', noise=3, indir=outdir+obj+'/spire/advanced_products/cube/',
-            plotdir=outdir+obj+'/spire/advanced_products/contours/', objname=obj,
-            spire=1, brightness=1)
+# # #
+# # The option of wavelength shift for testing the false-positive rate
+# if 'FPtest' in reduction_name:
+#     wl_shift = 6.8
+# else:
+#     wl_shift = 0
+#
+# for o in obsid_spire:
+#     # for running a certain source
+#     if o != obsid_spire[obj_list_spire.index('HH100')]:
+#         continue
+#     obj = obj_list_spire[obsid_spire.index(o)]
+#     print 'Step 3 - ', obj
+#     # read in RA/Dec
+#     radec_slw = ascii.read(outdir+obj+'/spire/data/cube/'+obj+'_radec_slw.txt')
+#     radec_ssw = ascii.read(outdir+obj+'/spire/data/cube/'+obj+'_radec_ssw.txt')
+#     # SLW
+#     idl.pro('extract_spire', indir=outdir+obj+'/spire/data/cube/', outdir=outdir+obj+'/spire/advanced_products/cube/',
+#             plotdir=outdir+obj+'/spire/advanced_products/cube/plots/', localbaseline=10, global_noise=20,
+#             ra=radec_slw['RA(deg)'].data, dec=radec_slw['Dec(deg)'].data, coordpix=radec_slw['Pixel'].data,
+#             slw=1, noiselevel=3, brightness=1, object=obj, flat=1, continuum=1, current_pix=1, double_gauss=1,
+#             print_all=outdir+reduction_name+'_spire_lines', wl_shift=wl_shift)
+#     # SSW
+#     idl.pro('extract_spire', indir=outdir+obj+'/spire/data/cube/', outdir=outdir+obj+'/spire/advanced_products/cube/',
+#             plotdir=outdir+obj+'/spire/advanced_products/cube/plots/', localbaseline=10, global_noise=20,
+#             ra=radec_ssw['RA(deg)'].data, dec=radec_ssw['Dec(deg)'].data, coordpix=radec_ssw['Pixel'].data,
+#             ssw=1, noiselevel=3, brightness=1, object=obj, flat=1, continuum=1, current_pix=1, double_gauss=1,
+#             print_all=outdir+reduction_name+'_spire_lines', wl_shift=wl_shift)
+#
+#     # make contour plots
+#     idl.pro('plot_contour', noise=3, indir=outdir+obj+'/spire/advanced_products/cube/',
+#             plotdir=outdir+obj+'/spire/advanced_products/contours/', objname=obj,
+#             spire=1, brightness=1)
 
 ###################### STEP 4 ######################
 # re-format the SECT-reduced 1-D product and perform fitting
 # print 'Step 4'
-SPIRE1D_run(obsid=obsid, indir=outdir, outdir=outdir, global_dir=outdir+reduction_name+'_spire_1d', wl_shift=wl_shift)
+# SPIRE1D_run(obsid=obsid, indir=outdir, outdir=outdir, global_dir=outdir+reduction_name+'_spire_1d', wl_shift=wl_shift)
 
 ###################### STEP 5 ######################
 # Fit alpha for three photometric bands for later measuring photometric fluxes.
@@ -309,14 +309,14 @@ SPIRE1D_run(obsid=obsid, indir=outdir, outdir=outdir, global_dir=outdir+reductio
 
 ###################### STEP 4 ######################
 # combine the line fitting results of PACS and SPIRE spectra
-# foo = open(outdir+reduction_name+'_lines.txt', 'w')
-# foo.write('{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s} \n'.format(*header))
-#
-# pacs_line = open(outdir+reduction_name+'_pacs_lines.txt', 'r').readlines()
-# spire_line = open(outdir+reduction_name+'_spire_lines.txt', 'r').readlines()
-#
-# for p in range(len(pacs_line)):
-#     foo.write(pacs_line[p])
-# for s in range(len(spire_line)):
-#     foo.write(spire_line[s])
-# foo.close()
+foo = open(outdir+reduction_name+'_lines.txt', 'w')
+foo.write('{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s}{:>20s} \n'.format(*header))
+
+pacs_line = open(outdir+reduction_name+'_pacs_lines.txt', 'r').readlines()
+spire_line = open(outdir+reduction_name+'_spire_lines.txt', 'r').readlines()
+
+for p in range(len(pacs_line)):
+    foo.write(pacs_line[p])
+for s in range(len(spire_line)):
+    foo.write(spire_line[s])
+foo.close()
